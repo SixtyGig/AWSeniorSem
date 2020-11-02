9fcb2a01137f278d922de51ab56c3b8514e7023c
SixtyGig <austin.winkler@cuw.edu>
2020-11-02 02:59:26 -0600

    Setting up ItemHoming (for out-of-bounds items)

---
+++ Assets/Scripts/MainMenu/homing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homing : MonoBehaviour
+{
+    private Vector3 pos;
+    private Quaternion rot;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        pos = this.transform.position;
+        rot = this.transform.rotation;
+    } 
+
+    void GoHome() 
+    {
+        this.gameObject.transform.position = pos;
+        this.gameObject.transform.rotation = rot;
+    }
+}
---
+++ Assets/SendObjectHome.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendObjectHome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ImportantObject") 
+        {
+            other.GetComponent<homing>.SendHome();
+        }
+    }
+}
