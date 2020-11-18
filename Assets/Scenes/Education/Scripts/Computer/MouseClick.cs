848e4513135ddf268ed9d92ccc686280477a420e
SixtyGig <austin.winkler@cuw.edu>
2020-11-17 21:57:35 -0600

    Reorganized Internal Struture & New VR Progress
    - File structure reorganized to make navigation easier
    - Added progress for VR Input
    - Minor MainMenu changes (moving around assets to fix visual bugs)

---
+++ Assets/Scenes/Education/Scripts/Computer/MouseClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MouseClick : MonoBehaviour
+{
+    [Header("SteamVR Input Sources")]
+    public SteamVR_Input_Sources leftHand;
+    public SteamVR_Input_Sources rightHand;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GrabGrip
+        if (SteamVR_Input.GetStateDown("GrabGrip", leftHand))
+        {
+            Debug.Log("LeftHand GrabGrip");
+        }
+        if (SteamVR_Input.GetStateDown("GrabGrip", rightHand)) 
+        {
+        
+        }
+
+        // Trigger (Mouse Click)
+    }
+}
